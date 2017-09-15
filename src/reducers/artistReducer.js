f3f092ffe50431fd880bc5c8af55d832cf62c1f1
juan-bastidas <juan.bastidas@mentor360.net>
2017-09-15 17:08:51 -0500

    Update artistReducer.js

--- src/reducers/artistReducer.js
+++ src/reducers/artistReducer.js
@@ -1,9 +1,22 @@
 import * as types from '../actions/actionTypes';
+
+/*
+Deberia definir un default state asi se sabe la estructura de lo que va a contener
+*/
+
 export default function ArtistReducer(state=[], action){
   switch(action.type){
     case types.LOAD_ARTISTS_SUCCESS:
       return action.artists;
       case types.CREATE_ARTIST_SUCCESS:
+      /*
+      El assign sobra, puede hacer simplemente esto:
+      
+            return {
+                ...state,
+                artist: action.artist,
+            };
+      */
         return [
          ...state,
          Object.assign({}, action.artist)
@@ -14,6 +27,11 @@ export default function ArtistReducer(state=[], action){
         Object.assign({}, action.artist)
       ];
       case types.DELETE_ARTIST_SUCCESS:
+      /*
+      Aqui esta mutando el estado sino estoy mal, deberia hacer una copia, pero normalmente un eliminar no se hace asi.
+      Basicamente pueden haber mas usuarios eliminando y lo que debe hacer es un llamado a otra accion en el success para
+      traer nuevamente los artistas del API.
+      */
       return [
         ...state.filter(artist => artist.Id !== action.artist.Id)
       ];
