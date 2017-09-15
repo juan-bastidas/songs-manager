ec17d9ddd6bae12358843714dbcab27e4e467545
juan-bastidas <juan.bastidas@mentor360.net>
2017-09-15 16:58:41 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import './styles/styles.css';
 import configureStore from './store/configureStore';
 import {loadSongs} from './actions/songActions';
 import {loadArtists} from './actions/artistActions';
+
+//No es la forma de importar, nunca referencie node_modules
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
